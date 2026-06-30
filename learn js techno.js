85eb6640d9f499f1c0dd955b351bfadd4e447f05
Anushka Shukla <anushka.shukla1708@gmail.com>
2026-06-30 14:12:36 +0530

    Added complete bootcamp project

---
+++ learn js techno.js
@@ -0,0 +1,19 @@
+/*function hello(){
+    document.write( "box1 box")
+}
+hello();
+
+const hello=()=>{     arrow function
+    same work
+}
+
+function techno(a,b){     anonmous function
+    return a+b;
+}
+document.write(techno(4,4));
+
+EventS
+DOM
+var element=document;
+console.log(element);*/
+
